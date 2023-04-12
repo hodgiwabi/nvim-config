73943ef94f2f4694f819106c87b58821c90fe5bc
hodgiwabi <thodgespdx@gmail.com>
2023-04-11 21:42:35 -0700

    Add obsidian.nvim

---
+++ plugins/obsidian.lua
@@ -0,0 +1,10 @@
+return {
+  "epwalsh/obsidian.nvim",
+  opts = {
+    dir = "/Users/hodgest/Library/Mobile Documents/iCloud~md~obsidian/Documents/Autodesk/",
+    completion = {
+      nvim_cmp = true,
+    },
+  },
+  ft = { "markdown" },
+}
